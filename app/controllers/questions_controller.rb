8ce9909f51203e23df7dcabd12f1e1c1a5780763
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2018-10-24 16:51:58 -0500

    ENH:Added controller method for the main page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,12 @@
 class QuestionsController < ApplicationController
 
+  def getStarted
+    @question_array = []
+    @question = Question.select(:questions).first
+    @answers = Question.pluck(:answer, :option2, :option3, :option4).first
+    @question_array << @question
+    @question_array << @answers.shuffle
+    redirect_to '/questions'
+  end
+
 end
---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe QuestionsController do
+  describe 'getting started' do
+
+  end
+end
