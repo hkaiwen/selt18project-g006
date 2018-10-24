b2ff9fea60862b9641c093af2bd19a53f2d97d03
Avanti0532 <avantiharsih-deshmukh@uiowa.com>
2018-10-24 18:37:14 -0500

    ENH: Added the index method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,10 @@
 class QuestionsController < ApplicationController
-def index 
+def index
+  @ques_opt  = []
   @options   = Question.pluck(:answer,:option2,:option3,:option4).first.shuffle
   @questions = Question.pluck(:questions).first
+  @ques_opt << @questions
+  @ques_opt << @options
 end
 
 def new
