928a55d2136d6f26deb7f97ebdc4eac698087216
Karan Pahlani <karan@wireless-nat-inside.uiowa.edu>
2018-10-24 11:45:52 -0500

    ENH: cr_q.rb, seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+questions = [
+  {:questions => 'efficacy means:', :answer => 'capacity or power to produce a desired result', :option2 => 'the state
+   of being restored to a former condition', :option3 => 'good-natured tolerance of delay or incompetence',
+   :option4 => 'the act of concealing something from the public', :explanation => 'The degree to which a method or
+   medicine brings about a specific result is its efficacy. You might not like to eat it, but you cant question the
+   efficacy of broccoli as a health benefit.'},
+
+
+  {:questions => 'pragmatic means:', :answer => 'alterable', :option2 => 'realistic', :option3 => 'relaxing',
+   :option4 => 'domesticated', :explanation => 'To describe a person or a solution that takes a realistic approach,
+  consider the adjective pragmatic. The four-year-old who wants a unicorn for her birthday isnt being very pragmatic.'},
+
+  {:questions => 'efficacy means:', :answer => 'capacity or power to produce a desired result', :option2 => 'the state
+   of being restored to a former condition', :option3 => 'good-natured tolerance of delay or incompetence',
+   :option4 => 'the act of concealing something from the public', :explanation => 'The degree to which a method or
+   medicine brings about a specific result is its efficacy. You might not like to eat it, but you cant question the
+   efficacy of broccoli as a health benefit.'},
+
+
+]
+
+questions.each do |question|
+  CreateQuestions.change!(question)
+end
