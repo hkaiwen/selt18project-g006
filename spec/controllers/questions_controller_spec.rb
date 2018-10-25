6a7d333392750a49e8d2e93912bf894336218291
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2018-10-24 21:42:58 -0500

    ENH:Added initial test steps for verifying answer

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe QuestionsController do
-  describe 'getting started' do
+  describe 'verifying answer' do
+    it 'should get the values from view'
+    it 'should call the appropriate model method to verify answer'
+    it 'should render index template' do
+
+    end
+
+
+
+
 
   end
 end
