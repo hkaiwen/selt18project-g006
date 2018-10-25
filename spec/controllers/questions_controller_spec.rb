a0e887eefd44d4e11e2d195afb1c82084fda6b3d
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2018-10-25 10:07:19 -0500

    Added test for checking if the result array is available to view

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -19,6 +19,14 @@ describe QuestionsController do
       post :submit_answer, { :question => 'pragmatic means', :optradio => 'alterable'}
       expect(response).to redirect_to('/questions')
     end
+    it 'should make the verified results to index template' do
+      @checking_array = ['pragmatic means', 'alterable']
+      fake_results = ['correct', 'alterable', 'To describe a person or a solution that takes a realistic approach,
+  consider the adjective pragmatic.']
+      expect(Question).to receive(:verify_answer).with(@checking_array).and_return(fake_results)
+      post :submit_answer, { :question => 'pragmatic means', :optradio => 'alterable'}
+      expect(assigns(:reply_array)).to eq(fake_results)
+    end
     it 'should check for null values from view' do
       post :submit_answer, { :opt_radio => ''}
       expect(flash[:notice]).to be_present
