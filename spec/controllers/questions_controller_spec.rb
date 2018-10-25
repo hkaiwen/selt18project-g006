5b5418050d6e5419784fc4d4726e0c0b66ce44ae
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2018-10-25 10:18:19 -0500

    Added context for valid and invalid entry in spec file

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,38 +3,37 @@ require 'rails_helper'
 
 describe QuestionsController do
   describe 'verifying answer' do
-    it 'should get the values from view' do
-      @checking_array = ['pragmatic means', 'alterable']
-      expect(Question).to receive(:verify_answer).with(@checking_array)
-      post :submit_answer, {:question => 'pragmatic means', :optradio => 'alterable' }
+    context 'valid entry' do
+      it 'should get the values from view' do
+        @checking_array = ['pragmatic means', 'alterable']
+        expect(Question).to receive(:verify_answer).with(@checking_array)
+        post :submit_answer, {:question => 'pragmatic means', :optradio => 'alterable' }
+      end
+      it 'should call the appropriate model method to verify answer' do
+        @checking_array = ['pragmatic means', 'alterable']
+        fake_results = ['correct', 'alterable', 'To describe a person or a solution that takes a realistic approach,
+           consider the adjective pragmatic.']
+        expect(Question).to receive(:verify_answer).with(@checking_array).and_return(fake_results)
+        post :submit_answer, { :question => 'pragmatic means', :optradio => 'alterable'}
+      end
+      it 'should render index template' do
+        post :submit_answer, { :question => 'pragmatic means', :optradio => 'alterable'}
+        expect(response).to redirect_to('/questions')
+      end
+      it 'should make the verified results to index template' do
+        @checking_array = ['pragmatic means', 'alterable']
+        fake_results = [{:value => anything, :answer => anything, :description => anything}]
+        expect(Question).to receive(:verify_answer).with(@checking_array).and_return(fake_results)
+        post :submit_answer, { :question => 'pragmatic means', :optradio => 'alterable'}
+        expect(assigns(:reply_array)).to eq(fake_results)
+      end
     end
-    it 'should call the appropriate model method to verify answer' do
-      @checking_array = ['pragmatic means', 'alterable']
-      fake_results = ['correct', 'alterable', 'To describe a person or a solution that takes a realistic approach,
-  consider the adjective pragmatic.']
-      expect(Question).to receive(:verify_answer).with(@checking_array).and_return(fake_results)
-      post :submit_answer, { :question => 'pragmatic means', :optradio => 'alterable'}
+    context 'invalid entry' do
+      it 'should check for null values from view' do
+        post :submit_answer, { :opt_radio => ''}
+        expect(flash[:notice]).to be_present
+        expect(response).to redirect_to('/questions')
+      end
     end
-    it 'should render index template' do
-      post :submit_answer, { :question => 'pragmatic means', :optradio => 'alterable'}
-      expect(response).to redirect_to('/questions')
-    end
-    it 'should make the verified results to index template' do
-      @checking_array = ['pragmatic means', 'alterable']
-      fake_results = [{:value => anything, :answer => anything, :description => anything}]
-      expect(Question).to receive(:verify_answer).with(@checking_array).and_return(fake_results)
-      post :submit_answer, { :question => 'pragmatic means', :optradio => 'alterable'}
-      expect(assigns(:reply_array)).to eq(fake_results)
-    end
-    it 'should check for null values from view' do
-      post :submit_answer, { :opt_radio => ''}
-      expect(flash[:notice]).to be_present
-      expect(response).to redirect_to('/questions')
-    end
-
-
-
-
-
   end
 end
