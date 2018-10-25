4ba09a85de33f23741792ab95814a98e8f996e96
Karan Pahlani <karan@wireless-nat-guest-inside.uiowa.edu>
2018-10-25 11:42:48 -0500

    ENH: verify_ans

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,4 +9,16 @@ class Question < ActiveRecord::Base
     Question::create!(question: question, option2: option2, option3: option3, option4: option4, explanation: explanation)
   end
 
+  def self.verify_answer(checking_array)
+    ques = Question.find_by(questions: checking_array[0])
+    hash = Hash.new
+
+    if(ques.answer == checking_array[1])
+      hash[:value] = 'correct'
+    else
+      hash[:value] = 'incorrect'
+    end
+
+  end
+
 end
