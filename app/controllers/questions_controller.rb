0fddc6bc116f73a6bb3fe00c2ad073ed1eedf9f5
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2018-10-24 22:02:22 -0500

    ENH:Added spec and starter code to check for retrieving values from view

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,14 @@ class QuestionsController < ApplicationController
 
   end
 
-
+  # method to call verifyAnswer model method
+  def submit_answer
+    @checking_array = []
+    @question = params[:question]
+    @answer = params[:answer]
+    @checking_array << @question << @answer
+    Question.verify_answer(@checking_array)
+    redirect_to questions_path
+  end
 
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 
 describe QuestionsController do
   describe 'verifying answer' do
-    it 'should get the values from view'
+    it 'should get the values from view' do
+      expect(Question).to receive(:verify_answer).with(:checking_array)
+      post :submit_answer, {:question => 'pragmatic means', :answer => 'alterable' }
+    end
     it 'should call the appropriate model method to verify answer'
     it 'should render index template' do
       post :submit_answer, { :question => ''}
