cec688d2cb4fdf80d7ce68852099d78f8db10b2b
Karan Pahlani <karan@Karans-MacBook-Pro.local>
2018-10-25 19:46:12 -0500

    ENH: @visited

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,20 +9,25 @@ class QuestionsController < ApplicationController
   # Model method would in turm return if the answer is correct or wrong along with the right answer for both the
   # cases and also 1/4th of the explanation to be displayed in an array  of hashes
   # PS: Am still working on the controller method(not yet fully implemented)
+  #
+  #added a visited variable to checking_array
+  # need to add a condition to check @visited = 0/1 before displaying question
   def submit_answer
+    @visited = 0
     @checking_array = []
     @question = params[:question]
     @answer = params[:optradio]
-    @checking_array << @question << @answer
+    @checking_array << @question << @answer << @visited
     if @checking_array.all? {|a| a.nil?}
       flash[:notice] = 'Please select an answer'
     else
+      @checking_array[2] = 1
       @reply_array = Question.verify_answer(@checking_array)
       @reply_array.each do |hash|
         if hash[:value] == 'correct'
           flash[:notice] = 'Great!Your answer is correct'
         else
-           flash[:notice] = 'Sorry.This is the correct answer'
+           flash[:notice] = 'Sorry.This is the incorrect answer'
         end
       end
     end
--- app/models/question.rb
+++ app/models/question.rb
@@ -9,15 +9,21 @@ class Question < ActiveRecord::Base
     Question::create!(question: question, option2: option2, option3: option3, option4: option4, explanation: explanation)
   end
 
+#  def self.visited_question(checking_array)
+#      checking_array[2] = 1
+ # end
+
   def self.verify_answer(checking_array)
     ques = Question.find_by(questions: checking_array[0])
     hash = Hash.new
-
-    if(ques.answer == checking_array[1])
+    if ques.answer == checking_array[1]
       hash[:value] = 'correct'
     else
       hash[:value] = 'incorrect'
     end
   end
+
+
+
 end
 
