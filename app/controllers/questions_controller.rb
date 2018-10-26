b2c5563b0c6a4a1b074fd19470b8092a08e04b18
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2018-10-26 12:51:06 -0500

    Changed the condition check for selecting nothing

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,39 +1,39 @@
 class QuestionsController < ApplicationController
- def index
-  @ques_opt  = []
-  @questions = Question.pluck(:questions,:answer,:option2,:option3,:option4).sample
-  @options = @questions.slice(1..4).shuffle
-  @ques_opt << @questions[0]
-  @ques_opt << @options
-  @ques_opt.flatten!
- end
+  def index
+    @ques_opt  = []
+    @questions = Question.pluck(:questions,:answer,:option2,:option3,:option4).sample
+    @options = @questions.slice(1..4).shuffle
+    @ques_opt << @questions[0]
+    @ques_opt << @options
+    @ques_opt.flatten!
+  end
 
- def new
-  #empty method
- end
+  def new
+    #empty method
+  end
 
- def show
-  @explain = Question.where(questions: params[:question]).pluck('explanation')
-  @new_explain = @explain[0].scan(/\.(.*)/)
- end
+  def show
+    @explain = Question.where(questions: params[:question]).pluck('explanation')
+    @new_explain = @explain[0].scan(/\.(.*)/)
+  end
 
   def submit_answer
     @checking_array = []
     @question = params[:question]
     @answer = params[:optradio]
     @checking_array << @question << @answer
-    if @checking_array.all? {|a| a.nil?}
+    if @checking_array.any? {|a| a.nil?}
       flash[:notice] = 'Please select an answer'
     else
       @reply_array = Question.verify_answer(@checking_array)
-      puts @reply_array
-
-        if @reply_array == 'correct'
+      @reply_array.each do |hash|
+        if hash.value == 'correct'
+          puts 'inside if'
           flash[:notice] = 'Great!Your answer is correct'
         else
-           flash[:notice] = 'Sorry.This is the incorrect answer'
+          flash[:notice] = 'Sorry.This is the incorrect answer'
         end
-
+      end
     end
     redirect_to questions_path
   end
