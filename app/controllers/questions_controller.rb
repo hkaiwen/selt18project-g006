04bdcf44ae2ca15c24d0c306b8c97efd65921e7b
Avanti0532 <avantiharsih-deshmukh@uiowa.com>
2018-10-26 12:42:57 -0500

    ENH: updated the question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,27 @@
 class QuestionsController < ApplicationController
- def index
-  @ques_opt  = []
-  @questions = Question.pluck(:questions,:answer,:option2,:option3,:option4).sample
-  @options = @questions.slice(1..4).shuffle
-  @ques_opt << @questions[0]
-  @ques_opt << @options
-  @ques_opt.flatten!
- end
+  @@tot_ques = []
+  def index
+   @ques_opt  = []
+   @questions = Question.pluck(:questions,:answer,:option2,:option3,:option4).sample
+   if @@tot_ques.include?(@question[0])
+     @questions = Question.pluck(:questions,:answer,:option2,:option3,:option4).sample
+   else 
+     @@tot_ques << @questions[0]
+   end
+   @options = @questions.slice(1..4).shuffle
+   @ques_opt << @questions[0]
+   @ques_opt << @options
+   @ques_opt.flatten!
+  end
 
- def new
-  #empty method
- end
+  def new
+   #empty method
+  end
 
- def show
-  @explain = Question.where(questions: params[:question]).pluck('explanation')
-  @new_explain = @explain[0].scan(/\.(.*)/)
- end
+  def show
+   @explain = Question.where(questions: params[:question]).pluck('explanation')
+   @new_explain = @explain[0].scan(/\.(.*)/)
+  end
 
   # method to call verify_answer model method with an array as parameter.
   # Array consists of question and the answer selected by the user.
