69e0b1b20398b8ca8e8044d48e8cd07b887180e2
linhpha <linh-t-pham@uiowa.edu>
2018-10-30 15:53:29 -0500

    Add cucumber

--- features/step_definitions/question_steps.rb
+++ features/step_definitions/question_steps.rb
@@ -10,18 +10,23 @@ end
 
 When /^I click on 'Next' button/ do
   @current_question
-  page.all('h1').each do |a|
-    if Question.find_by_questions(a.text) != nil
-      @previous_question = a.text
-    end
-  end
+  @previous_question = page.find('#question').text
   click_button('Next')
 end
 
 Then /^I should get a new question$/ do
-  page.all('h1').each do |a|
-    if Question.find_by_questions(a.text) != nil
-      expect a.text != @previous_question
+  current_question = page.find('#question').text
+    if Question.find_by_questions(current_question) != nil
+      expect current_question != @previous_question
     end
   end
+
+
+Then /^I should see a question and 4 options$/ do
+  current_question_text = page.find('#question').text
+  current_question = Question.find_by_questions(current_question_text)
+  page.all('ul li').each do |option|
+  expect (option == current_question.answer) || (option == current_question.option2) || (option == current_question.option3) || (option == current_question.option4)
+  end
+  expect Question.find_by_questions(current_question) != nil
 end
