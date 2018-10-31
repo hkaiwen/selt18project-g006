8b91202f8e79d206f9bb8256c3101a7a05ba30fe
Karan Pahlani <karan@wireless-nat-inside.uiowa.edu>
2018-10-31 11:53:57 -0500

    ENH: cucumber test get start button

--- features/step_definitions/question_steps.rb
+++ features/step_definitions/question_steps.rb
@@ -1,10 +1,20 @@
 
+Given /the following questions have been added to Question:/ do |question_table|
+  question_table.hashes.each do |question|
+    Question.create(question)
+  end
+end
+
+Given /^I am on the question page$/ do
+  visit questions_path
+end
+
 When /^I click on 'Get Start' button$/ do
   visit root_path
   click_button("Get Start")
 end
 
 Then /^I should be able to the play the game$/ do
-  url = URI.parse(current_url)
-  url.include?("questions")
+  str = current_url
+  expect(str.include?("question")).to be_truthy
 end
