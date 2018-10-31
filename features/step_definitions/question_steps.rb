ea4c6525c34fd0022c60aad5c392b632769e6c79
Karan Pahlani <karan@Karans-MacBook-Pro.local>
2018-10-30 22:29:52 -0500

    ENH: get start

--- features/step_definitions/question_steps.rb
+++ features/step_definitions/question_steps.rb
@@ -1,3 +1,10 @@
+
 When /^I click on 'Get Start' button$/ do
+  visit root_path
   click_button("Get Start")
+end
+
+Then /^I should be able to the play the game$/ do
+  url = URI.parse(current_url)
+  url.include?("questions")
 end
