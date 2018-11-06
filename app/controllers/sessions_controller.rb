339857d029509d4c5657dc1c56f045c4aecccd74
linhpha <linh-t-pham@uiowa.edu>
2018-11-06 17:44:04 -0600

    Add login and signup form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,8 @@ class SessionsController < ApplicationController
 
   end
 
-  def create
-      redirect_to questions_path
-    else
-      flash[:notice] = 'Login Failed.'
-      redirect_to login_path
-    end
-  end
-
   def destroy
-    #destroy session
+    #destroy sessions
 
   end
 end
