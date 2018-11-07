ce5bf7f7936eb74d3df9918daccf66af60447050
Karan Pahlani <karan@wireless-nat-inside.uiowa.edu>
2018-11-07 14:08:42 -0600

    populated controllet method fuctions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+#sign in and redirect to show page
+      session[:session_token]= user.session_token
       redirect_to questions_path
     else
-      flash[:notice] = 'Login Failed.'
+      flash.now[:warning] = 'Invalid email/password'
       redirect_to login_path
     end
-  end
 
-  def destroy
+    def destroy
     #destroy session
+
+    end
 
   end
 end
