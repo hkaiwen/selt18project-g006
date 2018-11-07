858da0997e831baa1f1cc56b0838938c73fcd407
Karan Pahlani <karan-pahlani@uiowa.edu>
2018-11-07 14:16:26 -0600

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,6 @@ class SessionsController < Devise::SessionsController
   def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-#sign in and redirect to show page
       session[:session_token]= user.session_token
       redirect_to questions_path
     else
