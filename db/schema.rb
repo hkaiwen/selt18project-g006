003ee09d5889a2a9ae0be3856fd2841b19d0603e
Kaiwen Huang <kaiwen-huang@uiowa.edu>
2018-11-07 20:26:09 -0600

    add sign out button and redirect to question after signing in

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-  ActiveRecord::Schema.define(version: 20181107232939) do
-
+ActiveRecord::Schema.define(version: 20181107232939) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "questions"
@@ -43,4 +42,5 @@
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
 end
