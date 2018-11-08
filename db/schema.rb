e8f92c2a55ec9df7a5f96d093265af6401094c76
Karan Pahlani <karan@wireless-nat-inside.uiowa.edu>
2018-11-07 18:00:15 -0600

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181106140718) do
+ActiveRecord::Schema.define(version: 20181107232939) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "questions"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 20181106140718) do
     t.string   "session_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
