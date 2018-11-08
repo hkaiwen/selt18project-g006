f967f1bb74f95411c42b87be4675d7fae475ee86
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2018-11-07 18:57:27 -0600

    Added create method for calling model method to add a question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,13 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
+
   @@count = 0
   @@tot_ques = []
+
+
   def index
-    @ques_opt  = []
+    @ques_opt = []
     if params[:same]== 'yes' and params[:commit]== 'Submit'
       @ques_opt=params[:question]
       if !params[:explanation].nil?
@@ -34,6 +39,10 @@ class QuestionsController < ApplicationController
 
   def new
     #empty method
+  end
+
+  def create
+
   end
 
   def submit_answer
