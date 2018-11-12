0f239e54b29a57b34257505a69b5c1b862423feb
Karan Pahlani <karan@wireless-nat-guest-inside.uiowa.edu>
2018-11-12 16:44:55 -0600

    removed uneccesary methods to increase test coverage

--- app/controllers/DeviseController/sessions_controller.rb
+++ app/controllers/DeviseController/sessions_controller.rb
@@ -3,27 +3,6 @@
 class DeviseController::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  def create
-    user = User.find_by_email(params[:session][:email])
-    puts "in create session"
-    #super
-    if user && user.authenticate(params[:session][:password])
-      session[:session_token]= user.session_token
-      redirect_to questions_path
-    else
-      flash[:warning] = 'Invalid email/password'
-      redirect_to login_path
-    end
-  end
-
-  def destroy
-    session[:session_token]=nil
-    @current_user=nil
-    flash[:notice]= 'Logged out of your account'
-    redirect_to questions_path
-  end
-
-
 
   
   # GET /resource/sign_in
