d1b57de20ba099b0e094a310253c79987385393e
linhpha <linh-t-pham@uiowa.edu>
2018-11-23 12:40:56 -0600

    Add button to admin site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def admin?
+    current_user.try(:admin?)
+    # I made up the line above. Implement your own checks according to your setup
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  get '/dashboard' => "dashboard#index", :as => :dashboard
   devise_for :users
   # resources :questions
   # root :to => redirect('/questions')
