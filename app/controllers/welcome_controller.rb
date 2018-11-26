98b018e6e7be198a6a3272ac334b0ff4a416dadf
Kaiwen Huang <kaiwen-huang@uiowa.edu>
2018-11-26 14:47:46 -0600

    redirect to admin page after closing tag and come back again

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,9 @@ class WelcomeController < ApplicationController
   def index
 
   end
+  def landing
+    if current_user.first_name.to_s == 'admin'
+      redirect_to rails_admin_path
+    end
+  end
 end
