acb8a9100cc30e8df4fb611b68e156477675f540
linhpha <linh-t-pham@uiowa.edu>
2018-11-30 21:32:38 -0600

    Remove unnecesary print

--- app/assets/javascripts/question.js
+++ app/assets/javascripts/question.js
@@ -1,8 +1,6 @@
 $(document).ready( function () {
 
   $('#select').change(function () {
-    selectVal = $('#select').val();
-    console.log(selectVal);
     $('#select_level').prop("disabled", false);
   })
 
