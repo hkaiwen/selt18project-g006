29beca441248c3598467e8a5116e654c2231684e
linhpha <linh-t-pham@uiowa.edu>
2018-12-01 19:57:32 -0600

    Fix leaderboard controller spec

--- spec/controllers/leaderboard_controller_spec.rb
+++ spec/controllers/leaderboard_controller_spec.rb
@@ -9,12 +9,7 @@ describe LeaderboardController do
       @top_score = [['Robin', 'Hood', 20], ['Calvin', 'Klein', 10]]
     end
     it 'should call the model method to get the list of higher scores' do
-      #@leaderboard = mock(Leaderboard)
-      #expect(@leaderboard).to receive(:get_users).and_return(@top_score)
-      #Leaderboard.any_instance.should_receive(:get_users).and_return(@top_score)
-      #expect(Leaderboard.get_users).to receive(User.order(score: :desc).pluck).with(:first_name, :last_name, :id).and_return(@top_score)
-      #expect(Leaderboard.get_users).to be_eql(@top_score)
-      expect(leaderboard).to receive(user).and_return(@top_score)
+      expect(Leaderboard).to receive(:get_users).and_return(@top_score)
       get :index
     end
     it 'should make the values available to the view'
