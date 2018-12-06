e12f7eac098186a600beb94ecae8f42954fa45bf
SELT171920 <hemalatha-ramachandran@uiowa.edu>
2018-12-06 15:08:45 -0600

    Remove print statements

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -5,7 +5,6 @@ class Leaderboard
   def self.get_users
     @final_names = []
     @names = User.order(score: :desc).limit(10).pluck(:first_name,:last_name,:score)
-    puts "Name: #{@names}"
     @names.each do |name|
       if name[2] != 0
         full_name = name[0].inspect.gsub('"','') + ' ' + name[1].inspect.gsub('"','')
