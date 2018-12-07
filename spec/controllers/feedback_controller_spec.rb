afe8f04e7c1ebfde143b667b460dbec8e1792c36
linhpha <linh-t-pham@uiowa.edu>
2018-12-06 20:35:05 -0600

    Fix rspec

--- spec/controllers/feedback_controller_spec.rb
+++ spec/controllers/feedback_controller_spec.rb
@@ -2,6 +2,31 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe FeedbackController do
+  describe 'show feedback' do
+    before :each do
+      @feedback_result = [['feedback_text', 'test user', '2018-11-11', 5]]
+      @feedback = Feedback.new(:feedback_text => 'feedback_text', :user_id => 1, :created_at => '2018-11-11', :rating => 5 )
+      @feedback.save
+      @user = User.new(id: 1, first_name: 'test',last_name: 'user',email: 'temp@gmail.com', password: '123456', score: 2)
+      @user.save
+      @admin = User.new(id: 1, first_name: 'test',last_name: 'user',email: 'temp@gmail.com', password: '123456', score: 2, admin: true)
+      @admin.save
+    end
+
+    it 'should make filter out admin feedback and assign to feedback_data' do
+      allow_any_instance_of(FeedbackController).to receive(:current_user).and_return(@user)
+      allow(Feedback.joins(User)).to receive(:pluck).and_return(@feedback_result)
+      get :show
+      expect(assigns(:feedback_data)).to eq(@feedback_result)
+    end
+    it 'should return all feedback and assign to feedback_data' do
+      allow_any_instance_of(FeedbackController).to receive(:current_user).and_return(@admin)
+      allow(Feedback.joins(User)).to receive(:pluck).and_return(@feedback_result)
+      get :show
+      expect(assigns(:feedback_data)).to eq(@feedback_result)
+    end
+  end
+
   describe 'Create feedback' do
     let(:feedback) { build :feedback, post: post }
     before :each do
